5f64e33c93039697901960aead7a45d9c30bdc9f
joshweinstock <jyweinstock@uchicago.edu>
2025-02-10 21:17:01 +0000

    rails_calc2

---
+++ app/controllers/division_controller.rb
@@ -0,0 +1,14 @@
+class DivisionController < ApplicationController
+  def show_division_form
+    render({ :template => "divide_form" })
+  end
+
+  def divide
+    @first_number = params.fetch("first_number")
+    @second_number = params.fetch("second_number").to_f
+
+    @result = @ first_number / @second_number
+
+    render({ :template => "divide_results" })
+  end
+end
